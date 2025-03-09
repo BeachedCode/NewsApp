4e46deaff6666332cfa00f79c289d2cf57780740
Ethan <Beached@mail.uc.edu>
2025-03-08 20:43:30 -0500

    Finished NewsListPage

--- NewsApp/Pages/NewsListPage.xaml.cs
+++ NewsApp/Pages/NewsListPage.xaml.cs
@@ -1,11 +1,32 @@
 using NewsApp.Models;
+using NewsApp.Services;
 
 namespace NewsApp.Pages;
 
 public partial class NewsListPage : ContentPage
 {
-	public NewsListPage(Category item)
+    public List<Article> ArticleList;
+    private string selectedGenre;
+    public NewsListPage(Category item)
 	{
 		InitializeComponent();
-	}
+        selectedGenre = item.Name;
+        GetBreakingNews();
+        ArticleList = new List<Article>();
+    }
+    private async Task GetBreakingNews()
+    {
+        var apiService = new ApiService();
+        var newsResult = await apiService.GetNews(selectedGenre);
+        foreach (var item in newsResult.Articles)
+        {
+            ArticleList.Add(item);
+        }
+        CvNews.ItemsSource = ArticleList;
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopToRootAsync();
+    }
 }
